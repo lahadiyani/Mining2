a680bf72fdf9e15a2d8882102772762b23bf32e0
Hadiani <80145828+lahadiyani@users.noreply.github.com>
2024-01-08 17:05:57 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$useragent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
+$useragent = "xxx";
 
-$cookie = "googtrans=/en/id; googtrans=/en/id; login=1; user=589559707079-36.83.75.15; show_nt=1; Hm_lvt_2b147ccaeef7e49f5f9553cadfdf8428=1704647303; Hm_lpvt_2b147ccaeef7e49f5f9553cadfdf8428=1704647330";
+$cookie = "xxx";
 
